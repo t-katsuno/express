43090f35b119757b34df75b92f9fc11070594325
t-katsuno <2001015045se@cyber-u.ac.jp>
2024-07-21 21:02:56 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -2,10 +2,9 @@ var express= require('express');
 var router= express.Router();
 
 
-
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-const uri= "";
+const uri= "mongodb+srv://admin:admin@cluster0.whx4st5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client= new MongoClient(uri);
 
 router.get('/', async(req, res) =>{
